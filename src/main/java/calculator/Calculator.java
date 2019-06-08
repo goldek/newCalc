ecc073bac3548647f9f4ed674381071b3cc2f6ee
student5 <student5@pcoip.ki.agh.edu.pl>
2019-06-08 15:15:30 +0200

    removed main method

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,9 +2,6 @@ package calculator;
 
 public class Calculator {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
 
     public int add(int a, int b) {
         return a+b;
