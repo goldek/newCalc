8fbfa7690a22447351aa7dad50c84f5dbf8a6799
student5 <student5@pcoip.ki.agh.edu.pl>
2019-06-08 14:42:17 +0200

    Pierwzy commit

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,24 @@
+package calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int substract(int p, int p1) {
+        return p-p1;
+    }
+
+    public int multiply(int p, int p1) {
+        return p*p1;
+    }
+
+    public double divide(int p, int p1) {
+        return (double) p/p1;
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,37 @@
+package calculator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calc = new Calculator();
+
+    @Before
+    public void setUp() {
+        Calculator calc = new Calculator();
+    }
+
+    @Test
+    public void testAdd(){
+        assertEquals(5, calc.add(3,2));
+    }
+
+    @Test
+    public void testSubstract(){
+        assertEquals(7, calc.substract(17,10));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(25, calc.multiply(5,5));
+        assertEquals(0, calc.multiply(0,50));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals(10, calc.divide(100,10), 0.0001);
+    }
+}
